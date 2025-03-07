7bdcaddddf7769d44cf756010ffec9c590c77a99
Ramez Chreide <ramiz_chreide@outlook.com>
2025-03-07 12:36:33 +0100

    update: change tags

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,18 +4,14 @@ import "./Navbar.css";
 export default function Navbar() {
     return (
         <nav className="navbar">
-            <ul className="navbar-list">
-                <li>
-                    <NavLink to="/" className="navbar-link">
-                        Home
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink to="/characters" className="navbar-link">
-                        Characters
-                    </NavLink>
-                </li>
-            </ul>
+            <div className="navbar-list">
+                <NavLink to="/" className="navbar-link">
+                    Home
+                </NavLink>
+                <NavLink to="/characters" className="navbar-link">
+                    Characters
+                </NavLink>
+            </div>
         </nav>
     );
 }
